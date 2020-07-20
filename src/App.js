dd51501627fd16dd9c243d7d59606aedcc77c4cd
me-anshu7 <me.anshu7@gmail.com>
2020-07-20 15:39:09 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Board from './components/Board';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +27,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
